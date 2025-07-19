6f72aa7b1423ea43a61db5cfe6f21fe89dc27b87
yordanos. <yordinius7@gmail.com>
2025-07-19 13:34:45 -0700

    message

--- test.py
+++ test.py
@@ -1,6 +1,1 @@
 print("hellow world")
-git init                # Only if not already a git repo
-git add .               # Stage all changes
-git commit -m "message" # Commit your changes
-git remote add origin <your-repo-url>  # Only once per project
-git push -u origin main # Push to GitHub
